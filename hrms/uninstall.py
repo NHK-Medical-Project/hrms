e4dcab2b3f725b6585ee1cddd0c355f57c517d75
360ithubdev <dev@360ithub.com>
2024-12-05 12:06:51 +0000

    from live 5.12.24

--- hrms/uninstall.py
+++ hrms/uninstall.py
@@ -1,12 +1,12 @@
 import click
 
-from hrms.setup import before_uninstall as remove_custom_fields
+# from hrms.setup import before_uninstall as remove_custom_fields
 
 
 def before_uninstall():
 	try:
 		print("Removing customizations created by the Frappe HR app...")
-		remove_custom_fields()
+		#remove_custom_fields()
 
 	except Exception as e:
 		BUG_REPORT_URL = "https://github.com/frappe/hrms/issues/new"
